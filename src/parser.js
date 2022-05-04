c4c6f32df31efa230191d6468f183a5132431c45
Jacob Bundren <jacob.bundren@tpginc.com>
2022-05-04 15:40:17 -0500

    finish parser

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,41 @@
-import fs from 'fs';
-
 export default class Parser {
 
-    constructor() {
-
+    constructor(vueFiles = []) {
+        this.vueComponents = vueFiles;
+        this.parsedComponents = [];
     }
+
+    traverseComponents() {
+        if (this.vueComponents.length > 0) {
+            this.vueComponents.forEach((component) => {
+                if (component.componentName && component.componentTemplate) {
+                    this.parseComponent(component)
+                } else {
+                    throw new Error("Component missing properties to parse")
+                }
+            })
+        } else {
+            throw new Error("No components to parse");
+        }
+    }
+
+    parseComponent(unparsedComponent) {
+        let attrRegex = /data-cy="(.+?)"/gm
+        let valRegex = /"(.+)"/
+        let parsedComponent = {};
+        let matchedSelectors = unparsedComponent.componentTemplate.match(attrRegex)
+        if (matchedSelectors) {
+            parsedComponent = {
+                componentName: unparsedComponent.componentName,
+                selectors: []
+            };
+            matchedSelectors.forEach((selector) => {
+                parsedComponent.selectors.push(selector.match(valRegex)[1]);
+            })
+            this.parsedComponents.push(parsedComponent);
+        }
+    }
+
+
+
 }
